1255b8417de6e6e48370fff20c9ba0f38153ebae
intersim <emily.intersimone@gmail.com>
2017-11-11 22:56:00 -0500

    add about modal, refactor

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 // get the date
-const today = (new Date()).getDate();
-let selected = today;
+var today = (new Date()).getDate();
+var selected = today;
 
 // create video
-let width = window.innerWidth;
-let height = window.innerHeight;
+var width = window.innerWidth;
+var height = window.innerHeight;
 
-const [ iframe ] = document.getElementsByTagName('iframe');
+var iframe = document.getElementsByTagName('iframe')[0];
 
 iframe.setAttribute('width', width);
 iframe.setAttribute('height', height);
 
-const url = videos[today] + '?rel=0&amp;controls=0&amp;showinfo=0;autoplay=1';
+var url = 'https://www.youtube.com/embed/' + videos[today] + '?rel=0&amp;controls=0&amp;showinfo=0;autoplay=1';
 
 iframe.setAttribute('src', url);
 
-window.onresize = function() {
+window.onresize = function () {
   width = window.innerWidth;
   height = window.innerHeight;
 
@@ -24,29 +24,70 @@ window.onresize = function() {
 }
 
 // create numbers
-const numbersContainer = document.getElementById('numbers');
+var numbersContainer = document.getElementById('numbers');
 numbersContainer.onclick = changeVideoCallback;
 
-function changeVideoCallback (event) {
-  const number = event.target.innerText;
+function changeVideoCallback(event) {
+  var number = event.target.innerText;
   if (number > today) return;
-  
-  const dayElement = document.getElementById(number);
-  const prevSelectedElement = document.getElementById(selected);
+
+  var dayElement = document.getElementById(number);
+  var prevSelectedElement = document.getElementById(selected);
 
   prevSelectedElement.classList.remove('selected');
   selected = number;
   dayElement.classList.add('selected');
 
-  const url = videos[number] + '?rel=0&amp;controls=0&amp;showinfo=0;autoplay=1';
+  var url = 'https://www.youtube.com/embed/' + videos[number] + '?rel=0&amp;controls=0&amp;showinfo=0;autoplay=1';
   iframe.setAttribute('src', url);
 }
 
-for (let i = 1; i <= 25; i++) {
-  const dayElement = document.createElement('span');
+for (var i = 1; i <= 25; i++) {
+  var dayElement = document.createElement('span');
   dayElement.id = i;
+
   if (i <= today) dayElement.classList.add('active');
+  else dayElement.classList.add('inactive');
   if (i === today) dayElement.classList.add('selected');
+
   dayElement.innerText = i;
   numbersContainer.appendChild(dayElement);
+}
+
+// fade out numbers and buttons when mouse is out of window
+var bodyElement = document.getElementById('body');
+var numbersElement = document.getElementById('numbers')
+var aboutElement = document.getElementById('about-container');
+
+bodyElement.addEventListener("mouseenter", function () { fadeIn(numbersElement); fadeIn(aboutElement); });
+bodyElement.addEventListener("mouseleave", function () { fadeOut(numbersElement); fadeOut(aboutElement) });
+
+function fadeIn(element) {
+  element.classList.remove('disappear');
+  element.classList.add('appear');
+}
+
+function fadeOut(element) {
+  setTimeout(function () {
+    element.classList.add('disappear');
+    element.classList.remove('appear')
+  },
+    1000);
+}
+
+// show and hide the about modal
+var modalEl = document.getElementById('modal');
+var closeBtnEl = document.getElementById('modal-close');
+
+aboutElement.onclick = openModal;
+closeBtnEl.onclick = closeModal;
+
+function openModal() {
+  modalEl.classList.add('opened');
+  modalEl.classList.remove('closed');
+}
+
+function closeModal() {
+  modalEl.classList.remove('opened');
+  modalEl.classList.add('closed');
 }
